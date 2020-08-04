ba3eaf3f6c819d794d703207739804357195c8da
Peter Daum <daum@tyntec.com>
2020-08-04 21:09:53 +0200

    added reset all endpoint

--- wa-integrations/slack/config.js
+++ wa-integrations/slack/config.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const { v4: uuidv4 } = require('uuid');
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const appConfig = {
   TYNTEC_API: process.env.TYNTEC_API_KEY,
   SLACK_BOT: process.env.SLACK_BOT_TOKEN,
   MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/tyntec',
+  RESET_TOKEN: process.env.RESET_TOKEN || uuidv4(),
 };
 
 module.exports = appConfig;
