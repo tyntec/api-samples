b356887abf4e3c61c501b6700ab41d8d3c282a18
Martin Kodada <martinkodada@icloud.com>
2021-07-08 18:16:09 +0200

    Removes viber only integration

--- chat-integrations/viber-shopify-zendesk/routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const Shopify = require('./shopify')
-const Zendesk = require('./zendesk')
-
-module.exports = (app) => {
-  app.use('/shopify', Shopify)
-  app.use('/zendesk', Zendesk)
-}
