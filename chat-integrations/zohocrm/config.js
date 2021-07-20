7777ea279533a649d00dfa26d2045e0e048a9e1c
Martin Kodada <martinkodada@icloud.com>
2021-07-20 09:23:10 +0200

    removes Zoho Deluge dependency

--- chat-integrations/zohocrm/config.js
+++ chat-integrations/zohocrm/config.js
@@ -4,9 +4,10 @@ dotenv.config();
 
 const appConfig = {
 	PORT: process.env.PORT || 3000,
+	BASE_URL: process.env.BASE_URL,
 	CMD_API_KEY: process.env.CMD_API_KEY,
-	ZOHO_GET_FUNCTION_URL: process.env.ZOHO_GET_FUNCTION_URL,
-	ZOHO_UPDATE_FUNCTION_URL: process.env.ZOHO_UPDATE_FUNCTION_URL,
+	ZOHO_CLIENT_ID: process.env.ZOHO_CLIENT_ID,
+	ZOHO_CLIENT_SECRET: process.env.ZOHO_CLIENT_SECRET,
 	ZOHO_ORG_URL: process.env.ZOHO_ORG_URL,
 	CMD_CHANNEL_JID: `${process.env.WABA_NUMBER}@whatsapp.eazy.im`,
 };
--- chat-integrations/zohocrm/vendors/zoho.js
+++ chat-integrations/zohocrm/vendors/zoho.js
@@ -1,38 +1,86 @@
 const axios = require('axios');
 const appConfig = require('../config');
-const querystring = require('querystring');
+const openid = require('openid-client');
 
-async function getZohoContactDetails(mobileNumber) {
-  try {
-    const phonenumber = querystring.stringify({
-      phonenumber: mobileNumber
+const redirect_uri = `${appConfig.BASE_URL}/oauth/redirect`;
+
+class Client {
+  constructor() {
+    this.axios = axios.create();
+    this.openidClient = this.getOpenidClient();
+    this.refreshToken = null;
+  }
+
+  setAccountsUrl(accountsUrl) {
+    this.openidClient = this.getOpenidClient(accountsUrl);
+  }
+
+  authorizationUrl() {
+    return this.openidClient.authorizationUrl({
+      access_type: 'offline',
+      redirect_uri,
+      scope: 'ZohoCRM.modules.contacts.READ,ZohoCRM.modules.contacts.UPDATE'
     });
-    const URL = `${appConfig.ZOHO_GET_FUNCTION_URL}&${phonenumber}`;
-    await axios.default.get(URL);
-    //should return some 200
-  } catch (e) {
-    throw new Error(`Unable to fetch Zoho Contacts on: ${e}`);
+  }
+
+  callbackParams(input) {
+    return this.openidClient.callbackParams(input);
+  }
+
+  async generateTokensFromCallback(params) {
+    const tokenSet = await this.openidClient.oauthCallback(redirect_uri, params);
+    this.refreshToken = tokenSet.refresh_token;
+  }
+
+  async getContactDetails(phone) {
+    const response = await this.requestApi('get', '/crm/v2.1/Contacts/search', { params: { phone }});
+    return response.data.data[0];
+  }
+
+  async updateContactDetails(contactId, remarks) {
+    const [Email, Mailing_City, Mailing_Country] = remarks.toString().split('<br>');
+    await this.requestApi('put', `/crm/v2.1/Contacts/${contactId}`, { data: {
+      data: [{
+        Email,
+        Mailing_City,
+        Mailing_Country
+      }]
+    }});
+  }
+
+  getOpenidClient(accountsUrl) {
+    const issuer = new openid.Issuer({
+      authorization_endpoint: 'https://accounts.zoho.com/oauth/v2/auth',
+      token_endpoint: `${accountsUrl}/oauth/v2/token`
+    });
+    return new issuer.Client({
+      client_id: appConfig.ZOHO_CLIENT_ID,
+      client_secret: appConfig.ZOHO_CLIENT_SECRET
+    });
+  }
+
+  async requestApi(method, url, opts) {
+    const tokenSet = await this.openidClient.refresh(this.refreshToken);
+
+    const config = {
+      method,
+      baseURL: tokenSet.api_domain,
+      url,
+      headers: {
+        Authorization: `Zoho-oauthtoken ${tokenSet.access_token}`,
+      },
+    };
+    if (opts !== undefined && opts.params !== undefined) {
+      config.params = opts.params;
+    }
+    if (opts !== undefined && opts.data !== undefined) {
+      config.data = opts.data;
+    }
+
+    return await this.axios.request(config);
   }
 }
 
-async function updateZohoContactDetails(phonenumber, remarks) {
-  try {
-    const [email, city, country] = remarks.toString().split('<br>');
-    const query = querystring.stringify({
-      email,
-      city,
-      country
-    });
-    const phone = querystring.stringify({
-      phonenumber
-    });
-    let URL = `${appConfig.ZOHO_UPDATE_FUNCTION_URL}&${phone}&${query}`;
-    await axios.default.get(URL);
-  } catch (e) {
-    throw new Error(`Unable to fetch Zoho Contacts on: ${e}`);
-  }
-}
 module.exports = {
-  getZohoContactDetails,
-  updateZohoContactDetails
+  client: new Client()
 };
