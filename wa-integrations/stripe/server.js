f13d58ec805f2da2889f95663e6c1456004f8997
Radek Holý <radek.holy@applifting.cz>
2022-02-12 11:00:46 +0100

    wa-integrations/stripe: Add GET /success.html

--- wa-integrations/stripe/server.js
+++ wa-integrations/stripe/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.static('public'));
 app.listen(PORT, () =>
     console.log(`Server listening on port ${PORT}.`)
 );
