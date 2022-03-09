d77e5600b50d8edd68a5f8bc1adeb0a195247611
Radek Holý <radek.holy@applifting.cz>
2022-03-09 14:17:16 +0100

    wa-integrations/mews: Add POST /

--- wa-integrations/mews/README.md
+++ wa-integrations/mews/README.md
@@ -2,6 +2,22 @@
 
 A sample Mews-WhatsApp integration that allows ordering services using WhatsApp.
 
+## Requirements
+
+* A tyntec Conversations API key.
+* A WhatsApp Business Account (WABA) Number provided from tyntec.
+* Credentials (client token and access token) for a MEWS API environment of your choice.
+* A reservation in Mews that:
+  * starts in the past
+  * ends in the future
+  * is in the Started state (check-in)
+* A catalog of products that:
+  * both is connected to your WhatsApp Business Profile and is in Mews,
+  * that uses product IDs in Mews as content IDs in the Facebook catalog and
+  * uses the same Service ID for all products in Mews.
+* A mobile phone with the WhatsApp application not associated with your WABA.
+* A public URL of this app (e.g. a URL generated via ngrok).
+
 ## Installation
 
 Use the package manager install dependencies.
@@ -12,9 +28,17 @@ npm install
 
 ## Usage
 1. Copy the file .env.example to .env and set environment variables in .env:
+   * `MEWS_ACCESS_TOKEN` - The access token of this app provided by Mews, depends on property.
+   * `MEWS_CLIENT` - The name and version of this app.
+   * `MEWS_CLIENT_TOKEN` - The unique identifier of this app provided by Mews, depends on environment (testing, staging, production).
+   * `MEWS_PLATFORM_ADDRESS` - The base address of the MEWS API, depends on environment.
+   * `MEWS_SERVICE_ID` - The Mews Service ID of the products in the catalog.
    * `PORT` (optional) - Start an HTTP server listening for connections on the given port. Defaults to 3000.
+   * `TYNTEC_API_KEY` - Your tyntec API key.
 2. Start the server
 ```
 npm start
 ```
-3. Voilà!
+3. Register the public URL as your WhatsApp webhook.
+4. Send a message with a shopping cart and with the room number of the reservation in the body to your WhatsApp Business Account.
+5. Voilà!
--- wa-integrations/mews/server.js
+++ wa-integrations/mews/server.js
@@ -1,10 +1,87 @@
+const assert = require('assert');
+const axios = require('axios');
 const dotenv = require('dotenv');
 const express = require('express');
+const { composeAddOrderRequestAxiosConfig, composeGetAllReservationsRequestAxiosConfig, composeGetAllResourcesRequestAxiosConfig } = require('./src/mews');
+const { composeSendMessageRequestAxiosConfig } = require('./src/tyntec');
 
 dotenv.config();
+const MEWS_ACCESS_TOKEN = process.env.MEWS_ACCESS_TOKEN;
+const MEWS_CLIENT = process.env.MEWS_CLIENT;
+const MEWS_CLIENT_TOKEN = process.env.MEWS_CLIENT_TOKEN;
+const MEWS_PLATFORM_ADDRESS = process.env.MEWS_PLATFORM_ADDRESS;
+const MEWS_SERVICE_ID = process.env.MEWS_SERVICE_ID;
 const PORT = process.env.PORT || 3000;
+const TYNTEC_API_KEY = process.env.TYNTEC_API_KEY;
 
 const app = express();
+app.use(express.json());
+
+app.post('/', async function (req, res) {
+    const resourceName = req.body.order.text;
+    const ProductOrders = req.body.order.items.map(item => ({ ProductId: item.productId, Count: +item.quantity}));
+
+    const resourcesRequest = composeGetAllResourcesRequestAxiosConfig(
+        MEWS_PLATFORM_ADDRESS,
+        {
+            ClientToken: MEWS_CLIENT_TOKEN,
+            AccessToken: MEWS_ACCESS_TOKEN,
+            Client: MEWS_CLIENT,
+            Extent: { Resources: true }
+        }
+    );
+    const resourcesResponse = await axios(resourcesRequest);
+    const resources = resourcesResponse.data.Resources.filter(resource => resource.IsActive && resource.Name === resourceName && resource.Data.Discriminator === "Space");
+    assert.strictEqual(resources.length, 1);
+    const resourceId = resources[0].Id;
+
+    const reservationsRequest = composeGetAllReservationsRequestAxiosConfig(
+        MEWS_PLATFORM_ADDRESS,
+        {
+            ClientToken: MEWS_CLIENT_TOKEN,
+            AccessToken: MEWS_ACCESS_TOKEN,
+            Client: MEWS_CLIENT,
+            TimeFilter: 'Overlapping',
+            StartUtc: new Date().toISOString(),
+            EndUtc: new Date().toISOString(),
+            AssignedResourceIds: [resourceId],
+            States: ['Started'],
+            Extent: { Reservations: true }
+        }
+    );
+    const reservationsResponse = await axios(reservationsRequest);
+    assert.strictEqual(reservationsResponse.data.Reservations.length, 1);
+    const CustomerId = reservationsResponse.data.Reservations[0].CustomerId;
+
+    const orderRequest = composeAddOrderRequestAxiosConfig(
+        MEWS_PLATFORM_ADDRESS,
+        {
+            ClientToken: MEWS_CLIENT_TOKEN,
+            AccessToken: MEWS_ACCESS_TOKEN,
+            Client: MEWS_CLIENT,
+            CustomerId,
+            ServiceId: MEWS_SERVICE_ID,
+            ProductOrders
+        }
+    );
+    await axios(orderRequest);
+
+    const request = composeSendMessageRequestAxiosConfig(
+        TYNTEC_API_KEY,
+        {
+            from: req.body.to,
+            to: req.body.from,
+            channel: 'whatsapp',
+            content: {
+                contentType: 'text',
+                text: "We've received your order. We will get working on it immediately."
+            }
+        });
+    await axios(request);
+
+    res.sendStatus(204)
+})
+
 app.listen(PORT, () =>
     console.log(`Server listening on port ${PORT}.`)
 );
