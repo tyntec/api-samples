d48189ba81585462c128bf640443a334b9f4dd32
Radek Holý <radek.holy@applifting.cz>
2022-05-20 08:17:29 +0200

    sms-integrations/mews: Add a check-ins listener

--- sms-integrations/mews/README.md
+++ sms-integrations/mews/README.md
@@ -1,3 +1,37 @@
 # tyntec-mews-sms-demo
 
 A sample Mews-SMS integration that allows ordering products using SMS.
+
+## Requirements
+
+* A tyntec Conversations API key.
+* An SMS number provided from tyntec.
+* Credentials (client token and access token) for a Mews API environment of your choice.
+* An integration configured in Mews Operations to receive reservation events.
+* A customer with a phone number in Mews.
+* At least two active products in Mews.
+
+## Installation
+
+Use the package manager install dependencies.
+
+```
+npm install
+```
+
+## Usage
+1. Copy the file .env.example to .env and set environment variables in .env:
+  * `FROM` - The sender of the SMS messages.
+  * `MEWS_ACCESS_TOKEN` - The access token of this app provided by Mews, depends on property.
+  * `MEWS_CLIENT` - The name and version of this app.
+  * `MEWS_CLIENT_TOKEN` - The unique identifier of this app provided by Mews, depends on environment (testing, staging, production).
+  * `MEWS_PLATFORM_ADDRESS` - The base address of the Mews API, depends on environment.
+  * `MEWS_SERVICE_ID` - The Mews Service ID of the product to be ordered.
+  * `MEWS_WEBSOCKET_ADDRESS` - The base address for Mews WebSockets, depends on environment.
+  * `TYNTEC_API_KEY` - Your tyntec API key.
+2. Start the server
+```
+npm start
+```
+3. Check-in a reservation in Mews.
+4. Voilà!
---
+++ sms-integrations/mews/server.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+const ws = require('ws');
+const { ConversationsService } = require('./src/conversations');
+const { CustomersService } = require('./src/customers');
+const { ProductsService } = require('./src/products');
+
+(async () => {
+    dotenv.config();
+    const FROM = process.env.FROM;
+    const MEWS_ACCESS_TOKEN = process.env.MEWS_ACCESS_TOKEN;
+    const MEWS_CLIENT = process.env.MEWS_CLIENT;
+    const MEWS_CLIENT_TOKEN = process.env.MEWS_CLIENT_TOKEN;
+    const MEWS_PLATFORM_ADDRESS = process.env.MEWS_PLATFORM_ADDRESS;
+    const MEWS_SERVICE_ID = process.env.MEWS_SERVICE_ID;
+    const MEWS_WEBSOCKET_ADDRESS = process.env.MEWS_WEBSOCKET_ADDRESS;
+    const TYNTEC_API_KEY = process.env.TYNTEC_API_KEY;
+
+    const axiosInstance = axios.create();
+    const conversationsService = new ConversationsService(axiosInstance, TYNTEC_API_KEY);
+    const customersService = new CustomersService(axiosInstance, MEWS_PLATFORM_ADDRESS, MEWS_CLIENT_TOKEN, MEWS_ACCESS_TOKEN, MEWS_CLIENT);
+    const productsService = new ProductsService(axiosInstance, MEWS_PLATFORM_ADDRESS, MEWS_CLIENT_TOKEN, MEWS_ACCESS_TOKEN, MEWS_CLIENT);
+
+    const PRODUCTS = await productsService.getProductsByServices([MEWS_SERVICE_ID]);
+
+    const mewsWs = new ws.WebSocket(
+        MEWS_WEBSOCKET_ADDRESS,
+        {
+            headers: {
+                Cookie: `ClientToken=${MEWS_CLIENT_TOKEN}; AccessToken=${MEWS_ACCESS_TOKEN}`
+            }
+        }
+    );
+
+    mewsWs.on('message', async (data) => {
+        const reservationIds = JSON.parse(data).Events
+            .filter(event => event.Type === 'Reservation' && event.State === 'Started')
+            .map(event => event.Id);
+
+        if (!reservationIds.length) {
+            return;
+        }
+
+        const customers = await customersService.getCustomersByReservations(reservationIds);
+        for (const customer of customers) {
+            if (!customer.Phone) {
+                continue;
+            }
+
+            await conversationsService.sendMessage(FROM, customer.Phone, 'sms', {
+                contentType: 'text',
+                text: `You can order ${PRODUCTS[0].Name} by sending an SMS with text "1" or ${PRODUCTS[1].Name} by sending an SMS with text "2".`
+            });
+        }
+    });
+})();
