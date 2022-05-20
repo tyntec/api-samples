6bd698b7df5864715dfa9073ee4822298b2a5023
Radek Holý <radek.holy@applifting.cz>
2022-05-20 06:53:32 +0200

    sms-integrations/mews: Add ProductsService.getProductsByServices

--- sms-integrations/mews/src/products.js
+++ sms-integrations/mews/src/products.js
@@ -1,3 +1,5 @@
+const { composeGetAllProductsRequestAxiosConfig } = require('./mews');
+
 class ProductsService {
     constructor(axiosInstance, mewsPlatformAddress, mewsClientToken, mewsAccessToken, mewsClient) {
         this.axiosInstance = axiosInstance;
@@ -5,6 +7,20 @@ class ProductsService {
         this.mewsClientToken = mewsClientToken;
         this.mewsAccessToken = mewsAccessToken;
         this.mewsClient = mewsClient;
+    }
+
+    async getProductsByServices(ServiceIds) {
+        const request = composeGetAllProductsRequestAxiosConfig(
+            this.mewsPlatformAddress,
+            {
+                ClientToken: this.mewsClientToken,
+                AccessToken: this.mewsAccessToken,
+                Client: this.mewsClient,
+                ServiceIds
+            }
+        );
+        const response = await this.axiosInstance.request(request);
+        return response.data.Products;
     }
 }
 
