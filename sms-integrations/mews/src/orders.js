6360106a000a44010f11438c30876e133a602d6e
Radek Holý <radek.holy@applifting.cz>
2022-05-20 08:48:56 +0200

    sms-integrations/mews: Add OrdersService.createOrder

--- sms-integrations/mews/src/orders.js
+++ sms-integrations/mews/src/orders.js
@@ -1,3 +1,5 @@
+const { composeAddOrderRequestAxiosConfig } = require('./mews');
+
 class OrdersService {
     constructor(axiosInstance, mewsPlatformAddress, mewsClientToken, mewsAccessToken, mewsClient) {
         this.axiosInstance = axiosInstance;
@@ -5,6 +7,22 @@ class OrdersService {
         this.mewsClientToken = mewsClientToken;
         this.mewsAccessToken = mewsAccessToken;
         this.mewsClient = mewsClient;
+    }
+
+    async createOrder(CustomerId, ServiceId, ProductOrders) {
+        const request = composeAddOrderRequestAxiosConfig(
+            this.mewsPlatformAddress,
+            {
+                ClientToken: this.mewsClientToken,
+                AccessToken: this.mewsAccessToken,
+                Client: this.mewsClient,
+                CustomerId,
+                ServiceId,
+                ProductOrders
+            }
+        );
+        const response = await this.axiosInstance.request(request);
+        return response.data;
     }
 }
 
