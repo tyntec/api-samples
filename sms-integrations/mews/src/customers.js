352674b91d852f748fa6771e50843f0ca0c922ac
Radek Holý <radek.holy@applifting.cz>
2022-05-20 07:10:33 +0200

    sms-integrations/mews: Add CustomersService.getCustomersByReservations

--- sms-integrations/mews/src/customers.js
+++ sms-integrations/mews/src/customers.js
@@ -1,3 +1,5 @@
+const { composeGetAllCustomersRequestAxiosConfig, composeGetAllReservationsRequestAxiosConfig } = require("./mews");
+
 class CustomersService {
     constructor(axiosInstance, mewsPlatformAddress, mewsClientToken, mewsAccessToken, mewsClient) {
         this.axiosInstance = axiosInstance;
@@ -5,6 +7,40 @@ class CustomersService {
         this.mewsClientToken = mewsClientToken;
         this.mewsAccessToken = mewsAccessToken;
         this.mewsClient = mewsClient;
+    }
+
+    async getCustomersByReservations(ReservationIds) {
+        const reservationsRequest = composeGetAllReservationsRequestAxiosConfig(
+            this.mewsPlatformAddress,
+            {
+                ClientToken: this.mewsClientToken,
+                AccessToken: this.mewsAccessToken,
+                Client: this.mewsClient,
+                ReservationIds,
+                Extent: {
+                    Reservations: true
+                }
+            }
+        );
+        const reservationsResponse = await this.axiosInstance.request(reservationsRequest);
+        const CustomerIds = reservationsResponse.data.Reservations.map(reservation => reservation.CustomerId);
+
+        const customersRequest = composeGetAllCustomersRequestAxiosConfig(
+            this.mewsPlatformAddress,
+            {
+                ClientToken: this.mewsClientToken,
+                AccessToken: this.mewsAccessToken,
+                Client: this.mewsClient,
+                CustomerIds,
+                Extent: {
+                    Customers: true,
+                    Documents: false,
+                    Addresses: false
+                }
+            }
+        );
+        const customersResponse = await this.axiosInstance.request(customersRequest);
+        return customersResponse.data.Customers;
     }
 }
 
