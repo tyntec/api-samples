23985fb54b56f64f20961670ae71860565be72ad
Radek Holý <radek.holy@applifting.cz>
2022-05-20 07:19:18 +0200

    sms-integrations/mews: Add ConversationsService.sendMessage

--- sms-integrations/mews/src/conversations.js
+++ sms-integrations/mews/src/conversations.js
@@ -1,7 +1,22 @@
+const { composeSendMessageRequestAxiosConfig } = require('./tyntec');
+
 class ConversationsService {
     constructor(axiosInstance, tyntecApikey) {
         this.axiosInstance = axiosInstance;
         this.tyntecApikey = tyntecApikey;
+    }
+
+    async sendMessage(from, to, channel, content) {
+        const request = composeSendMessageRequestAxiosConfig(
+            this.tyntecApikey,
+            {
+                from,
+                to,
+                channel,
+                content
+            });
+        const response = await this.axiosInstance.request(request);
+        return response.data;
     }
 }
 
