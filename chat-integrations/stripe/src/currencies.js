5efe78f2f8bc02acfd6c923422a575dfd26ccf42
Radek Holý <radek.holy@applifting.cz>
2022-06-01 15:47:55 +0200

    chat-integrations/stripe: Add SMALLEST_UNIT constant

---
+++ chat-integrations/stripe/src/currencies.js
@@ -0,0 +1,12 @@
+// Please, adjust this map according to your preferred currencies supported by Stripe https://stripe.com/docs/currencies
+const SMALLEST_UNIT = {
+    AUD: 2,
+    EUR: 2,
+    GBP: 2,
+    JPY: 0,
+    USD: 2
+};
+
+module.exports = {
+    SMALLEST_UNIT
+};
